6210be081268327f836ed97f295760365a8cb524
mariam-01 <mariam.larhlid2001@gmail.com>
2025-07-01 19:24:41 +0100

    init project

---
+++ src/main/java/ma/bdcc/springmvc/SpringMvcApplication.java
@@ -0,0 +1,16 @@
+package ma.bdcc.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcApplication.class, args);
+        System.out.printf("hello");
+
+    }
+
+
+}
