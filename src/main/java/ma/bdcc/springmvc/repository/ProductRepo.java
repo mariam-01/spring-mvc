bdbbe831a5ca7b43fa433c69622aff47cb26816f
mariam-01 <mariam.larhlid2001@gmail.com>
2025-07-01 19:26:15 +0100

    add Product repository interface

---
+++ src/main/java/ma/bdcc/springmvc/repository/ProductRepo.java
@@ -0,0 +1,8 @@
+package ma.bdcc.springmvc.repository;
+
+import ma.bdcc.springmvc.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+
+}
