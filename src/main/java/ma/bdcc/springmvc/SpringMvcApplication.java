f3a81b69587708673c0f27f8ce05c22012dca191
mariam-01 <mariam.larhlid2001@gmail.com>
2025-07-01 19:26:49 +0100

    exclude Spring Security auto-configuration and add CommandLineRunner bean to initialize Product data

--- src/main/java/ma/bdcc/springmvc/SpringMvcApplication.java
+++ src/main/java/ma/bdcc/springmvc/SpringMvcApplication.java
@@ -1,9 +1,14 @@
 package ma.bdcc.springmvc;
 
+import ma.bdcc.springmvc.entities.Product;
+import ma.bdcc.springmvc.repository.ProductRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringMvcApplication {
 
     public static void main(String[] args) {
@@ -12,5 +17,18 @@ public class SpringMvcApplication {
 
     }
 
+    @Bean
+    CommandLineRunner start (ProductRepo productRepo) {
+        return args -> {
+            productRepo.save(Product.builder()
+                    .name("first product")
+                    .description("first product description")
+                    .quantity(100)
+                    .price(20)
+                    .build());
+            var products = productRepo.findAll();
+            products.forEach(System.out::println);
+        };
+    }
 
 }
